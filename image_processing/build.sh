eccecf118ec6c4742f06c00995bf02ebceca7206
Tom Koptel <tom.koptel@gmail.com>
2020-11-12 09:03:51 +0100

    Image Processing: Rework Theme1 project structure

---
+++ image_processing/build.sh
@@ -0,0 +1,33 @@
+
+# Pdflatex
+PDFLATEX='pdflatex -interaction=nonstopmode -shell-escape -file-line-error'
+GREP='grep ".*:[0-9]*:.*"' # показывает на выходе только ошибки
+
+# Файлы/Папки
+PDF_NAME='image_processing_theme1_koptel.pdf'
+TEX='tex'
+IMG='tex/inc/img'
+MAINTEX='0-main'
+
+# Конвертация eps
+find -E $IMG/ -type f -name "*.eps" -exec epstopdf {} ";" ;
+find -E $IMG -type f -name "*.eps" -exec rm -f {} \;
+
+# Сборка latex
+cd tex
+$PDFLATEX $MAINTEX > /dev/null
+BIBOUTPUT=$(bibtex $MAINTEX)
+# Показывать output bibtex'a только в случае ошибок
+if [[ "$BIBOUTPUT" =~ "error" ]]; then
+    echo "$BIBOUTPUT"
+fi
+$PDFLATEX $MAINTEX > /dev/null
+$PDFLATEX $MAINTEX
+
+cp $MAINTEX.pdf ../$PDF_NAME
+cd ..
+
+# Clear
+find -E $TEX/ -maxdepth 1 -type f ! -regex ".*\.(tex|log|blg|bib|cls|sty|bst|clo|asm|gitignore)" -exec rm -f {} \; ;
+
+

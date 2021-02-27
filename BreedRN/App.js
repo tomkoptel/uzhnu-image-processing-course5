213c73a9219c0cf13436b2cc5c5d8a80e1d427ab
tomkoptel <tom.koptel@gmail.com>
2021-02-27 16:50:21 +0100

    Setup React Native Project (#4)

---
+++ BreedRN/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: '58694a0f-wda1-471f-bd96-145571e29d72',
+    title: 'Fourth Item',
+  },
+  {
+    id: '58694a0w-wda1-471f-bd96-145571e29d72',
+    title: 'Fifth Item',
+  },
+  {
+    id: '58692a0w-wda1-471f-bd96-145571e29d72',
+    title: 'Sixth Item',
+  },
+  {
+    id: '58693a0w-wda1-471f-bd96-145571e29d72',
+    title: 'Seventh Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+export default function App() {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
---
+++ BreedRN/Readme.md
@@ -0,0 +1,3 @@
+# Start
+* Execute `npm install` in the root of the react native app.
+* Execute `npm run android` or `npm run ios`
